cb6b0c40dc70f4c3a5e16d5089e378c15fdaddea
DeryGermann <termaiter@gmail.com>
2020-08-05 15:16:14 -0600

    restarting...

--- ChessRepo/game.js
+++ ChessRepo/game.js
@@ -25,7 +25,7 @@ const readline = require('readline').createInterface({
 */
 
 let starting_state = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-let test_state = 'rnkbqbnr/pppppppp/8/8/8/8/PPPPPPPP/RNKBQBNR w KQkq - 0 1';
+
 let pawn = 'p';
 let bishop = 'b';
 let knight = 'n';
@@ -49,7 +49,7 @@ let choseGameState = (choice) => {
         if (choice == 1) {
             generateNormalChess();
         } else if (choice == 2) {
-            generateChess960();
+            generateStartingState();
         } else {
             console.log('*** Make sure you input the correct menu item. ***\n\nRerun the program');
         }
@@ -71,130 +71,3 @@ let generateNormalChess = () => {
     console.log(chess.ascii());
 }
 
-let generateChess960 = () => {
-    if (generateStartingState()) {
-        console.log('it worked.')
-    }
-}
-
-let generateStartingState = () => {
-    // Randomizes the starting state of the back row
-    let backrow_state = '';
-
-    // Intial starting state
-    for (let i = 0; i < 9; i++) {
-        // Checks if the piece being insertted already is or has more than two
-        switch (Math.floor(Math.random() * 5)) {
-            // Bishop has at most 2
-            case 0:
-                if (!backrow_state.match(`.*${bishop}.*${bishop}.*`)) {
-                    backrow_state += bishop;
-                }
-                break;
-            // Knight has at most 2
-            case 1:
-                if (!backrow_state.match(`.*${knight}.*${knight}.*`)) {
-                    backrow_state += knight;
-                }
-                break;
-            // Rook has at most 2
-            case 2:
-                if (!backrow_state.match(`.*${rook}.*${rook}.*`)) {
-                    backrow_state += rook;
-                }
-                break;
-            // Queen has at most 1
-            case 3:
-                if (!backrow_state.match(`.*${queen}.*`)) {
-                    backrow_state += queen;
-                }
-                break;
-            // King has at most 1
-            case 4:
-                if (!backrow_state.match(`.*${king}.*`)) {
-                    backrow_state += king;
-                }
-                break;
-            default:
-                console.log('Something went wrong when trying to insert chess pieces.')
-                break;
-        }
-    }
-
-    // Makes sure that the starting state contains each piece with the corrent amount per piece.
-    if (backrow_state.length < 8) {
-        if (!backrow_state.match(`.*${bishop}.*${bishop}.*`)) {
-            backrow_state += bishop;
-        }
-        if (!backrow_state.match(`.*${knight}.*${knight}.*`)) {
-            backrow_state += knight;
-        }
-        if (!backrow_state.match(`.*${rook}.*${rook}.*`)) {
-            backrow_state += rook;
-        }
-        if (!backrow_state.match(`.*${queen}.*`)) {
-            backrow_state += queen;
-        }
-        if (!backrow_state.match(`.*${king}.*`)) {
-            backrow_state += king;
-        }
-
-        // If this still does not work, recurssivly calls the method.
-        if (backrow_state.length < 8) generateStartingState();
-        // else console.log(`${backrow_state} :: ${backrow_state.length}`);
-        else findBishopPosition(backrow_state);
-    }
-}
-
-let findBishopPosition = (placedPieces) => {
-    /*
-        The other Bishop can only be 
-            1 square away,
-            3 squares away,
-            5 squares away,
-            7 squares away
-    */
-
-    let list = placedPieces.split("");
-    let firstBishopPosition = "k";
-    let secondBishopPostition;
-
-    // Gets the position of the first bishop
-    for (let index = 0; index < list.length; index++) {
-        if (list[index] == 'b') {
-            // Gets the position of the second bishop
-            if (firstBishopPosition == "k") {
-                firstBishopPosition = index;
-            } else {
-                secondBishopPostition = index;
-            }
-        }
-    }
-
-    // console.log(placedPieces);
-    // console.log(`${firstBishopPosition} :: ${secondBishopPostition}`);
-    moveBishopsIntoCorrectPositions(list, firstBishopPosition, secondBishopPostition);
-}
-
-let moveBishopsIntoCorrectPositions = (list, firstPos, secondPos) => {
-
-}
-// generateMenu();
-
-// UNIT TEST 1
-// Tests the starting state for Chess960
-// for (let index = 0; index < 10; index++) {
-//     generateStartingState();                    
-// }
-
-// UNIT TEST 2
-// Finds the position of each Bishop
-// for (let i = 0; i < 10; i++) {
-//     generateStartingState();
-// }
-
-// UNIT TEST 3
-// Puts the Bishops into rule appropriate positions
-for (let i = 0; i < 10; i++) {
-    generateStartingState();
-}
